354b12f74d24ec5f635144c4c54b5c050d9e816b
mrbitcoiner <mrbitcoiner@tutanota.com>
2022-09-27 09:52:06 -0300

    First script version

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#####################################
+# One command setup docker and docker-compose on ubuntu
+#
+# By Mr. Bitcoiner
+####################################
+# Script
+
+set -e
+
+sudo apt update
+
+sudo apt install -y \
+    apt-transport-https ca-certificates curl gnupg lsb-release
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+sudo printf "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\n" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update 
+
+sudo apt install -y \
+    docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+printf 'Setup done.\n'
